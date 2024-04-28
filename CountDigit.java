450fe733885dc1a895166ca966e450fc04e19e78
Isha139 <ishar810256@gmail.com>
2024-04-28 20:06:47 +0530

    Updated

--- CountDigit.java
+++ CountDigit.java
@@ -1,6 +1,25 @@
 public class CountDigit{
+    public static int countDigits(int n) {
+
+        int count = 0, givenNumber = n;
+
+        while (n > 0) {
+
+            if ((n % 10) != 0 && (givenNumber % (n % 10)) == 0) {
+
+                count++;
+
+            }
+
+            n /= 10;
+
+        }
+
+        return count;
+
+    }
     public static void main(String[] args) {
         int n = 336;
-       System.out.println(n);
+       System.out.println(countDigits(n));
     }
 }
