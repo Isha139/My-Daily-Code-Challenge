359570cbba36f6c3261b88571b0a7918ad189b48
Isha139 <ishar810256@gmail.com>
2024-07-04 17:31:28 +0530

    Tried another method

--- Q23.java
+++ Q23.java
@@ -1,88 +1,56 @@
 
 public class Q23 {
-   
-        void merge(int arr[], int l, int m, int r)
-        {
-            // Find sizes of two subarrays to be merged
-            int n1 = m - l + 1;
-            int n2 = r - m;
-    
-            // Create temp arrays
-            int L[] = new int[n1];
-            int R[] = new int[n2];
-    
-            // Copy data to temp arrays
-            for (int i = 0; i < n1; ++i)
-                L[i] = arr[l + i];
-            for (int j = 0; j < n2; ++j)
-                R[j] = arr[m + 1 + j];
-    
-            // Merge the temp arrays
-    
-            // Initial indices of first and second subarrays
-            int i = 0, j = 0;
-    
-            // Initial index of merged subarray array
-            int k = l;
-            while (i < n1 && j < n2) {
-                if (L[i] <= R[j]) {
-                    arr[k] = L[i];
-                    i++;
-                }
-                else {
-                    arr[k] = R[j];
-                    j++;
-                }
-                k++;
-            }
-    
-            // Copy remaining elements of L[] if any
-            while (i < n1) {
-                arr[k] = L[i];
-                i++;
-                k++;
-            }
-    
-            // Copy remaining elements of R[] if any
-            while (j < n2) {
-                arr[k] = R[j];
-                j++;
-                k++;
-            }
+
+    public static void conquer(int arr[], int l, int mid, int r){
+          int merged[] = new int[r - l + 1];
+
+          int i1 = l;
+          int i2 = mid+1;
+          int x = 0;
+
+          while (i1 <= mid && i2 <= r) {
+             if(arr[i1] <= arr[i2]){
+                merged[x++] = arr[i1++];
+             }else{
+                merged[x++] = arr[i2++];
+             }
+          }
+
+          while (i1 <= mid) {
+            merged[x++] = arr[i1++];
+          }
+
+          while(i2 <= r){
+            merged[x++] = arr[i2++];
+          }
+
+          for(int i=0, j=l; i<merged.length; i++, j++){
+            arr[j] = merged[i];
+          }
+    }
+
+     public static void divide(int arr[], int l, int r){
+        if(l >= r){
+            return;
         }
-    
-        void sort(int arr[], int l, int r)
-        {
-            if (l < r) {
-    
-                int m = l + (r - l) / 2;
-    
-                sort(arr, l, m);
-                sort(arr, m + 1, r);
-    
-                merge(arr, l, m, r);
-            }
-        }
-    
-    
-        static void printArray(int arr[])
-        {
-            int n = arr.length;
-            for (int i = 0; i < n; ++i)
-                System.out.print(arr[i] + " ");
-            System.out.println();
-        }
-    
+
+        int mid = l + (r-l)/2;
+        divide(arr, l, mid);
+        divide(arr, mid+1, r);
+        conquer(arr, l, mid, r);
+     }  
     public static void main(String args[]){
-        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        int arr[] = { 6, 3, 9, 5, 2, 8};
+        int n = arr.length;
 
-        System.out.println("Given array is");
-        printArray(arr);
-
-        Q23 ob = new Q23();
-        ob.sort(arr, 0, arr.length - 1);
-
-        System.out.println("\nSorted array is");
-        printArray(arr);
+        divide(arr, 0, n-1);
+        //print
+        for(int i =0; i<n; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
     }
 }
+
+// Output
+// 2 3 5 6 8 9 
