d2660a9e06e49a37606dc1db48faf8863a837077
Isha139 <ishar810256@gmail.com>
2024-07-04 19:02:07 +0530

    Revised

--- Q24.java
+++ Q24.java
@@ -1,60 +1,48 @@
 public class Q24 {
-    static void swap(int[] arr, int i, int j)
-    {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
 
-    static int partition(int[] arr, int low, int high)
-    {
-       
+    public static int partition(int arr[], int low, int high){
         int pivot = arr[high];
+        int i = low-1;
 
-        int i = (low - 1);
-
-        for (int j = low; j <= high - 1; j++) {
-
+        for(int j=low; j<high; j++){
             if (arr[j] < pivot) {
-
                 i++;
-                swap(arr, i, j);
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        swap(arr, i + 1, high);
-        return (i + 1);
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i; // pivot index
     }
+    
+    public static void quickSort(int arr[], int low, int high){
+        if(low < high){
+            int pidx = partition(arr, low, high);
 
-    static void quickSort(int[] arr, int low, int high)
-    {
-        if (low < high) {
-
-            int pi = partition(arr, low, high);
-
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+            quickSort(arr, low, pidx-1);
+            quickSort(arr, pidx+1, high);
         }
+         
     }
-    // To print sorted array
-    public static void printArr(int[] arr)
-    {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
     public static void main(String[] args)
     {
-        int[] arr = { 10, 7, 8, 9, 1, 5 };
-        int N = arr.length;
+        int[] arr = {6, 3, 9, 5, 2, 8};
+        int n = arr.length;
 
-        // Function call
-        quickSort(arr, 0, N - 1);
-        System.out.println("Sorted array:");
-        printArr(arr);
+        quickSort(arr, 0, n-1);
+
+       //print
+       for(int i=0; i<n; i++){
+        System.out.print(arr[i]+" ");
+       }
+       System.out.println();
     }
 }
 
-// Output:
-// Sorted array:
-// 1 5 7 8 9 10
+// Output
+// 2 3 5 6 8 9 
